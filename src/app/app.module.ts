1ce4f01a66b3ed92a8d1147f4e2f2f8fe7298355
Giuseppina Cirelli <75985013+morf83@users.noreply.github.com>
2022-03-23 10:17:40 +0100

    pulsanti share e ottimizzazione annunci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
 import { MenuComponent } from './menu/menu.component';
 import { LavoroComponent } from './lavoro/lavoro.component';
 import { RegaliComponent } from './regali/regali.component';
 import { ContattiComponent } from './contatti/contatti.component';
 import { HomeComponent } from './home/home.component';
-import {GalleriaModule} from 'primeng/galleria';
+import { GalleriaModule} from 'primeng/galleria';
 import { FooterComponent } from './footer/footer.component';
-import {ImageModule} from 'primeng/image';
-import {CarouselModule} from 'primeng/carousel';
-import {MatCardModule} from '@angular/material/card';
-import { regalo } from './regali/regalo';
+import { ImageModule} from 'primeng/image';
+import { CarouselModule} from 'primeng/carousel';
+import { MatCardModule} from '@angular/material/card';
+import { utente } from './utente';
+
 
 
 
@@ -30,6 +31,7 @@ import { regalo } from './regali/regalo';
     HomeComponent,
     FooterComponent,
 
+
   ],
   imports: [
     BrowserModule,
@@ -40,9 +42,11 @@ import { regalo } from './regali/regalo';
     GalleriaModule,
     ImageModule,
     CarouselModule,
-    MatCardModule
+    MatCardModule,
+
 
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/utente.ts
@@ -0,0 +1,7 @@
+export interface utente {
+  nome: string,
+  cognome: string,
+  email: string,
+  tel: string,
+  id: number
+}
