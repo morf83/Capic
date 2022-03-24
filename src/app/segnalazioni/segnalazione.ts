aa0ef5e4be3ddb7f1f813a4fe410bd09e3e26f8b
Giuseppina Cirelli <75985013+morf83@users.noreply.github.com>
2022-03-24 10:04:36 +0100

    segnalazione.ts

---
+++ src/app/segnalazioni/segnalazione.ts
@@ -0,0 +1,10 @@
+export interface Segnalazione {
+  id: 'number',
+  nome: 'string',
+  email: 'string',
+  telefono: 'string',
+  indirizzo: 'string',
+  codiceImmobile:'string',
+  titolo: 'string',
+  descrizione: 'string'
+}
