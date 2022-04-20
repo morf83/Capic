6adecc6ea59cfdd73d00027e59da1f655fb241d7
Giuseppina Cirelli <75985013+morf83@users.noreply.github.com>
2022-04-20 11:10:05 +0200

    1

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { REGALI } from './regali/regali';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  getRegali() {
+    return REGALI;
+  }
   constructor() { }
 }
--- src/app/regali/regali.component.ts
+++ src/app/regali/regali.component.ts
@@ -24,6 +24,7 @@ export class RegaliComponent implements OnInit {
     })
   }
 
+
   ngOnInit(): void {
   }
 
